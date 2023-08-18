a4396b240cf27352fa161af52960f6e53d9fdb5b
Nitesh Singh <69296248+ntssingh@users.noreply.github.com>
2023-08-18 16:50:08 +0530

    Update ErrorPage.jsx

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,34 +1,37 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-import styles from '../styles/styles'
 
 const ErrorPage = () => {
   return (
-    <div>
-    <div
-      style={{
-        width: "100%",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+  export default function Example() {
+  return (
+    <>
+      {/*
+        This example requires updating your template:
 
-        <h1>404-Page not found</h1>
-        <br /><br />
-
-        <div className={`${styles.button}`}>
-            <Link to="/">
-              <h1 className="text-[#fff] flex items-center">
-                Go to Homepage
+        ```
+        <html class="h-full">
+        <body class="h-full">
+        ```
+      */}
+      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-semibold text-indigo-600">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
+          <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Link to="/" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        
+                Go back homepage
                 
-              </h1>
             </Link>
           </div>
+        </div>
+      </main>
+    </>
+  )
+}
 
-    </div>
-    </div>
 
   )
 }
